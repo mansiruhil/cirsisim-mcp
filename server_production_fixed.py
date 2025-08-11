afd83209a45576dd8fee378b628d44f4bdcd8c87
mansi ruhil <157950124+mansiruhil@users.noreply.github.com>
2025-08-11 17:09:54 +0530

    Update server_production_fixed.py

--- server_production_fixed.py
+++ server_production_fixed.py
@@ -14,7 +14,7 @@ import openai
 from mcp.server import Server
 from mcp.server.models import InitializationOptions
 from mcp.server.stdio import stdio_server
-from mcp.server.models import NotificationOptions, ExperimentalCapabilities
+
 from mcp.types import (
     CallToolRequest,
     CallToolResult,
@@ -322,9 +322,10 @@ async def main():
                 server_name="CrisisSim",
                 server_version="1.0.0",
                 capabilities = server.get_capabilities(
-        notification_options=NotificationOptions(),
-    experimental_capabilities=ExperimentalCapabilities()
+    notification_options=None,
+    experimental_capabilities=None
 )
+
             ),
         )
 
